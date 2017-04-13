ccd079d12fb00319fabcc8c279079c14fa7145ec
Colby Chiang <colbychiang@wustl.edu>
2017-04-13 04:34:30 +0000

    checkpoint

--- bamfixflags.py
+++ bamfixflags.py
@@ -16,25 +16,140 @@ __author__ = "Colby Chiang (colbychiang@wustl.edu)"
 __version__ = "$Revision: 0.0.1 $"
 __date__ = "$Date: 2017-04-12 01:36 $"
 
+
+# get the number of entries in the set
+def countRecords(myCounter):
+    numRecords = sum(myCounter.values())
+    return numRecords
+
+# sum of the entries
+def sumRecords(myCounter):
+    mySum = 0.0
+    for c in myCounter:
+        mySum += c * float(myCounter[c])
+    return mySum
+
+# calculate percentiles from the depth counter.
+# uses the Nearest Rank definition of percentile
+def percentile(myCounter, p):
+    p = float(p)
+    if p <= 0 or p >= 100:
+        return
+    
+    #length is the number of bases we're looking at
+    numEntries = countRecords(myCounter)
+    # the ordinal value of the output element
+    limit = int(round(p/100 * numEntries + 0.5))
+
+    # a list of the values, sorted smallest to largest
+    # note that this list contains unique elements only
+    valueList = list(myCounter)
+    valueList.sort()
+    numValues = len(valueList)
+
+    # the number of entries through the set we've gone
+    runEntries = 0
+    # iterator i
+    i = 0
+    # initiate v, in case one element in input
+    v = valueList [i]
+
+    # move through the value list, iterating by number of
+    # entries for each value
+    while runEntries < limit:
+        v = valueList[i]
+        runEntries += myCounter[v]
+        i += 1
+        # if i is greater than numValues, just return the largest
+        if i == (numValues):
+            v = valueList[i - 1]
+            break
+    return v
+
+# calculate the arithmetic mean, given a counter and the
+# length of the feature (chromosome or genome)
+# for x percentile, x% of the elements in the set are
+# <= the output value
+def mean(myCounter):
+    # the number of total entries in the set is the
+    # sum of the occurrences for each value
+    numRecords = countRecords(myCounter)
+
+    # u holds the mean
+    u = float()
+
+    u = sumRecords(myCounter) / numRecords
+    return u
+
+def stdev(myCounter):
+    # the number of total entries in the set is the
+    # sum of the occurrences for each value
+    numRecords = countRecords(myCounter)
+
+    # u holds the mean
+    u = mean(myCounter)
+    sumVar = 0.0
+
+    # stdev is sqrt(sum((x-u)^2)/#elements)
+    for c in myCounter:
+        sumVar += myCounter[c] * (c - u)**2
+    myVariance = float(sumVar) / numRecords
+    stdev = myVariance**(0.5)
+    return stdev
+
+
+# # calculate the density curve for and insert size histogram
+# def calc_insert_density(self):
+#     dens = Counter()
+#     for i in list(self.hist):
+#         dens[i] = float(self.hist[i])/countRecords(self.hist)
+#     self.dens = dens
+        
 def bamfixflags(bamfile,
                 lib_info_file,
                 limit,
                 is_sam,
                 bam_out,
-                uncompressed_out):
+                uncompressed_out,
+                outlier_bound = 2.0,
+                mapping_bound = 3.0,
+                max_stddev = 4.0):
+
+    proper = {}
 
     lib_info = json.load(lib_info_file)
+    for sample in lib_info:
+        # print lib_info[sample]['libraryArray'][0]
+        for lib in lib_info[sample]['libraryArray']:
+            lib_hist = {int(k):int(v) for k,v in lib['histogram'].items()}
+            p25 = percentile(lib_hist, 25)
+            p75 = percentile(lib_hist, 75)
+            lib_mean = mean(lib_hist)
+            lib_sd = stdev(lib_hist)
 
-    lib = lib_info[0]['libraryArray'][0]
+            print 'p25', p25
+            print 'p75', p75
+            print 'mean', lib_mean
+            print 'sd', lib_sd
 
+            low = int(p25 - mapping_bound * (p75 - p25) + .499)
+            high = int(p75 + mapping_bound * (p75 - p25) + .499)
 
-    print lib
-        # convert the histogram keys to integers (from strings in JSON)
-        # lib_hist = {int(k):int(v) for k,v in lib['histogram'].items()}
+            if (low > high):
+                sys.stderr.write("Error: insert size upper bound is smaller than read length\n");
+                exit(1)
+            
+            # ensure z-score limits do not exceed IQR limits
+            low = min(low, int(lib_mean - max_stddev * lib_sd))
+            high = max(high, int(lib_mean + max_stddev * lib_sd))
+        
+            # minimum of zero
+            if (low < 0):
+                low = 0
 
-
-
-    print lib_info
+            # assign the readgroups
+            for rg in lib['readgroups']:
+                proper[rg] = [low, high]
 
     # set input file
     if bamfile == None: 
@@ -56,27 +171,63 @@ def bamfixflags(bamfile,
     else:
         out_bam = pysam.Samfile('-', 'wh', template=in_bam)
         
-
-    # parse readgroup string
-    try:
-        rg_list = readgroup.split(',')
-    except AttributeError:
-        rg_list = None
-
-    counter = 0
+    print proper
     for al in in_bam:
-        # must be in a user specified readgroup
-        if rg_list and al.opt('RG') not in rg_list:
-            continue
+        # out_bam.write(al)
+        print al
 
-        # write out alignment
-        out_bam.write(al)
-        counter += 1
+        if al.is_supplementary:
+            pass
+
+        elif al.is_unmapped or al.mate_is_unmapped:
+            if al.is_proper_pair:
+                print 'mismarked proper (unmapped)'
+            al.is_proper_pair = False
+
+        elif al.reference_id != al.next_reference_id:
+            if al.is_proper_pair:
+                print 'mismarked proper (chrom)'
+            al.is_proper_pair = False
+
+        elif (al.reference_start < al.next_reference_start
+            and (al.is_reverse or not al.mate_is_reverse)):
+            if al.is_proper_pair:
+                print 'mismarked proper (orient +)'
+            al.is_proper_pair = False
+
+        elif (al.reference_start > al.next_reference_start
+            and (not al.is_reverse or al.mate_is_reverse)):
+            if al.is_proper_pair:
+                print 'mismarked proper (orient -)'
+            al.is_proper_pair = False
+
+        # if al.supp
+        elif (al.template_length >= proper[al.opt('RG')][0]
+              and al.template_length <= proper[al.opt('RG')][1]):
+            if not al.is_proper_pair:
+                print 'mismarked improper (insert size)'
+            al.is_proper_pair = True
+        else:
+            if al.is_proper_pair:
+                print 'mismarked proper (insert size)'
+            al.is_proper_pair = False
+
+        # out_bam.write(al)
+        print al
+        # print proper[al.opt('RG')], al.template_length
+        # print al
+        # # must be in a user specified readgroup
+        # if al.opt('RG') not in rg_list:
+        #     continue
+
+        # # write out alignment
+        # out_bam.write(al)
+        # counter += 1
         
-        # bail if reached limit
-        if (limit != None
-            and counter >= limit):
-            break
+        # # bail if reached limit
+        # if (limit != None
+        #     and counter >= limit):
+        #     break
 
 
 # ============================================
